bca26a27ee903af9450a0f8fa35fe0b50db83723
Abraham Galue <abrahamgalue512@gmail.com>
2025-04-02 21:42:25 -0400

    Used generic locator

--- tests/uitesting.spec.js
+++ tests/uitesting.spec.js
@@ -2,9 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://uitestingplayground.com/');
-  await page.getByRole('heading', { name: 'UI Test Automation Playground' }).click();
-  await page.getByRole('link', { name: 'Resources' }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
-  await page.getByRole('link', { name: 'Click' }).click();
-  await page.getByRole('heading', { name: 'Click' }).click();
+
+  await page.locator('body').click()
+  await page.locator('#title').click()
+  await page.locator('p.mb-0').click()
+  await page.locator('.blockquote-footer').click()
+
+  await page.locator('a').filter({ hasText: 'Dynamic ID' }).click()
+
+  // await page.getByRole('heading', { name: 'UI Test Automation Playground' }).click();
+  // await page.getByRole('link', { name: 'Resources' }).click();
+  // await page.getByRole('link', { name: 'Home' }).click();
+  // await page.getByRole('link', { name: 'Click' }).click();
+  // await page.getByRole('heading', { name: 'Click' }).click();
 });
